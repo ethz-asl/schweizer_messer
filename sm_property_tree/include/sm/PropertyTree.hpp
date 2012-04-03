d1dbad9851a46539dc9cb63e39cb573f702092b6
furgalep <paul.furgale@gmail.com>
2012-04-03 13:24:42 +0200

    Added some comments and design rationalle to the property tree

--- sm_property_tree/include/sm/PropertyTree.hpp
+++ sm_property_tree/include/sm/PropertyTree.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file   PropertyTree.hpp
+ * @author Paul Furgale <paul.furgale@gmail.com>
+ * @date   Tue Apr  3 09:39:01 2012
+ * 
+ * @brief  A class that stores heirarchically orgainized properties for
+ *         easy initialization of other classes.
+ * 
+ *
+ * 
+ */
 #ifndef SM_PROPERTY_TREE_HPP
 #define SM_PROPERTY_TREE_HPP
 
@@ -8,6 +19,55 @@ namespace sm {
   // Forward declaration.
   class PropertyTreeImplementation;
   
+  
+  /**
+   * @class PropertyTree
+   * @brief  A class that stores heirarchically orgainized properties for
+   *         easy initialization of other classes.
+   * 
+   * This class is designed to hold properties organized heirarchically
+   * and intended for the initialization of classes. In the basic use
+   * case, a class would write a constructor accepting a property tree
+   * and use it to initialize internal properties:
+   *
+   * \code
+   * MyClass::MyClass(const PropertyTree & config)
+   * {
+   *    // Configuration "get" functions that are not given
+   *    // a default value must exist in the property tree
+   *    // or an exception will be thrown
+   *    _theta = config.getDouble("theta");
+   *    _numIterations = config.getInt("numIterations");
+   *
+   *    // Configuration "get" functions that are given
+   *    // a default value will not throw an exception
+   *    // if the key is not in the property tree.
+   *    _phi = config.getDouble("phi", 0.01);
+   *
+   * }
+   * \endcode
+   *
+   * If one class has a member variable of another class that must
+   * also be initialized, the member variable may be pointed to 
+   * properties lower in the property heirarchy by creating a new
+   * child property tree with a child namespace:
+   *
+   * \code
+   * ComposedClass::ComposedClass(const PropertyTree & config) :
+   *   _memberClass1(PropertyTree(config, "member1")), _memberClass2(PropertyTree(config, "member2"))
+   * {
+   *    // ...
+   * }
+   * \endcode
+   *
+   * The class is designed to take an implementation derived from
+   * PropertyTreeImplementaion. This should allow us to write
+   * implementation classes for ROS and ADTF, two middlewear platforms
+   * currently being used in robotics at ETHZ. A sample implementation
+   * using the boost::property_tree library is available in this
+   * package.
+   *
+   */
   class PropertyTree
   {
   public:
