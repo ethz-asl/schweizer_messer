ff3290c47f5d38dd7aef78cb0622e7d0df746f8c
furgalep <paul.furgale@gmail.com>
2012-10-12 13:20:36 +0200

    Added some tools to make it easier to build a python interface

---
+++ sm_python/include/sm/python/stl_converters.hpp
@@ -0,0 +1,31 @@
+#include<boost/python.hpp>
+
+namespace sm {
+    namespace python {
+        
+        /// \brief Convert an STL list to a boost::python list 
+        template<typename LIST_ITERATOR_T>
+        inline void stlToList(const LIST_ITERATOR_T & begin, const LIST_ITERATOR_T & end, boost::python::list & outlist)
+        {
+            LIST_ITERATOR_T it = begin;
+            boost::python::list outList;
+            for( ; it != end; ++it)
+            {
+                outList.append(*it);
+            }
+
+            
+        } 
+
+        template<typename MAP_ITERATOR_T>
+        inline void stlToDict(const MAP_ITERATOR_T & begin, const MAP_ITERATOR_T & end, boost::python::dict & out)
+        {
+            MAP_ITERATOR_T it = begin;
+            for( ; it != end; ++it)
+            {
+                out[it->first] = it->second;
+            }
+        }
+
+    } // namespace python
+} // namespace sm
