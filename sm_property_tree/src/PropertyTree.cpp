28089c2d42b29b72b55bc1c1b782b2312c78c820
Paul Furgale <paul.furgale@gmail.com>
2012-11-13 18:00:02 +0100

    Added some sanity checks in the property tree and compiled with debug symbols

--- sm_property_tree/src/PropertyTree.cpp
+++ sm_property_tree/src/PropertyTree.cpp
@@ -43,88 +43,105 @@ namespace sm {
     
   double PropertyTree::getDouble(const std::string & key) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getDouble(buildQualifiedKeyName(key));
   }
 
   double PropertyTree::getDouble(const std::string & key, double defaultValue) const
   {
-    return _imp->getDouble(buildQualifiedKeyName(key), defaultValue);
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
+      return _imp->getDouble(buildQualifiedKeyName(key), defaultValue);
   }
 
   double PropertyTree::getDouble(const std::string & key, double defaultValue)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getDouble(buildQualifiedKeyName(key), defaultValue);
   }
   
   int PropertyTree::getInt(const std::string & key) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getInt(buildQualifiedKeyName(key));
   }
 
   int PropertyTree::getInt(const std::string & key, int defaultValue) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getInt(buildQualifiedKeyName(key),defaultValue);
   }
 
   int PropertyTree::getInt(const std::string & key, int defaultValue)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getInt(buildQualifiedKeyName(key),defaultValue);
   }
 
   bool PropertyTree::getBool(const std::string & key) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getBool(buildQualifiedKeyName(key));
   }
 
   bool PropertyTree::getBool(const std::string & key, bool defaultValue) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getBool(buildQualifiedKeyName(key), defaultValue);
   }
 
   bool PropertyTree::getBool(const std::string & key, bool defaultValue) 
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getBool(buildQualifiedKeyName(key), defaultValue);
   }
 
   
   std::string PropertyTree::getString(const std::string & key) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getString(buildQualifiedKeyName(key));
   }
 
   std::string PropertyTree::getString(const std::string & key, const std::string & defaultValue) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getString(buildQualifiedKeyName(key), defaultValue);
   }
 
   std::string PropertyTree::getString(const std::string & key, const std::string & defaultValue) 
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->getString(buildQualifiedKeyName(key), defaultValue);
   }
   
   bool PropertyTree::doesKeyExist(const std::string & key) const
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->doesKeyExist(buildQualifiedKeyName(key));
   }
 
 
   void PropertyTree::setDouble(const std::string & key, double value)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->setDouble(buildQualifiedKeyName(key), value);
   }
 
   void PropertyTree::setInt(const std::string & key, int value)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->setInt(buildQualifiedKeyName(key), value);
   }
 
   void PropertyTree::setBool(const std::string & key, bool value)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->setBool(buildQualifiedKeyName(key), value);
   }
   
   void PropertyTree::setString(const std::string & key, const std::string & value)
   {
+      SM_ASSERT_TRUE(Exception, _imp, "The implementation is NULL");
     return _imp->setString(buildQualifiedKeyName(key), value);
   }
 
