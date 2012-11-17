30f18b1615e2f0cca27a975c78bd573344d54d70
Paul Furgale <paul.furgale@gmail.com>
2012-11-17 13:49:40 +0100

    Updated the serialized map to support multiple tables in one file

--- sm_database/include/sm/database/implementation/SerializedMap.hpp
+++ sm_database/include/sm/database/implementation/SerializedMap.hpp
@@ -3,41 +3,53 @@ namespace sm {
   namespace database {
     
     template<typename T, typename A>
-    SerializedMap<T,A>::SerializedMap(const ::boost::filesystem::path & dbFileName, const std::string & tableName) : _tableName(tableName), _db(NULL), _iStmt(NULL), _sStmt(NULL)
+    SerializedMap<T,A>::SerializedMap(const ::boost::filesystem::path & dbFileName, const std::string & tableName) : _tableName(tableName), _iStmt(NULL), _sStmt(NULL)
     {
-      SM_ASSERT_GT(InvalidDbNameException, dbFileName.string().size(), 0, "The database filename must be greater than zero");
-      validateTableName();
-      // Open a connection to the database.
-      // http://www.sqlite.org/c3ref/open.html
-      int flags = SQLITE_OPEN_READWRITE | // The connection is read/write
-	SQLITE_OPEN_CREATE;               // The database will be created if it doesn't exist. 
+        _db.reset(new Database(dbFileName));
 
-      int result = sqlite3_open_v2( dbFileName.string().c_str(), &_db, flags, NULL);
-      SM_ASSERT_TRUE(UnableToOpenDatabaseException, _db != NULL, "Unable to open sqlite3 database " << _dbName.string() << ", Error: " <<  sqlite3_errmsg(_db)); 
-      SM_ASSERT_EQ(UnableToOpenDatabaseException, result, SQLITE_OK, "Unable to open sqlite3 database " << _dbName.string() << ", Error: " <<  sqlite3_errmsg(_db));
-    
+        setUpTable();
+
+
+    }
+
+    template<typename T, typename A>
+    SerializedMap<T,A>::SerializedMap(boost::shared_ptr<Database> db, const std::string & tableName) : _tableName(tableName), _db(db), _iStmt(NULL), _sStmt(NULL)
+    {
+        SM_ASSERT_TRUE(UnableToOpenDatabaseException, _db.get() != NULL, "The database is null");
+
+        setUpTable();
+
+
+    }
+
+      
+      template<typename T, typename A>
+      void SerializedMap<T,A>::setUpTable()
+      {
+          validateTableName();
+          int result;
 
       // Make sure the table exists.
       // This table will have a 64 bit integer key and a binary blob of data.
       std::string sql = "create table if not exists " + _tableName + "(id INTEGER primary_key unique, data BLOB);";
       sqlite3_stmt * stmt;
       // http://www.sqlite.org/c3ref/prepare.html
-      result = sqlite3_prepare_v2(_db, sql.c_str(), -1, &stmt, NULL);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db));
+      result = sqlite3_prepare_v2(_db->db(), sql.c_str(), -1, &stmt, NULL);
+      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db->db()));
       result = sqlite3_step(stmt);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_DONE, "Unable to execute statement: " << sqlite3_errmsg(_db));
+      SM_ASSERT_EQ(SqlException, result, SQLITE_DONE, "Unable to execute statement: " << sqlite3_errmsg(_db->db()));
       // Finalize is like a delete statement. Every prepared statement must be finalized.
       sqlite3_finalize(stmt);
 
       // Pre-prepare the insert statement.
       std::string insert = "INSERT or REPLACE into " + _tableName + " VALUES(?,?);";
-      result = sqlite3_prepare(_db, insert.c_str(), -1, &_iStmt, 0);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db));
+      result = sqlite3_prepare(_db->db(), insert.c_str(), -1, &_iStmt, 0);
+      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db->db()));
     
       // Pre-prepare the select statement.
       std::string select = "SELECT data FROM " + _tableName + " WHERE id = ?";
-      result = sqlite3_prepare(_db, select.c_str(), -1, &_sStmt, 0);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db));
+      result = sqlite3_prepare(_db->db(), select.c_str(), -1, &_sStmt, 0);
+      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to prepare statement: " << sqlite3_errmsg(_db->db()));
 
       // \todo: http://web.utk.edu/~jplyon/sqlite/SQLite_optimization_FAQ.html
       // \todo: Set up an LRU cache.
@@ -51,7 +63,6 @@ namespace sm {
       // Finalize and close
       sqlite3_finalize(_iStmt);
       sqlite3_finalize(_sStmt);
-      sqlite3_close(_db);
     }
 
     
@@ -72,11 +83,11 @@ namespace sm {
 	
 	// Bind the id to the select statement.
 	result = sqlite3_bind_int64(_sStmt, 1, id);
-	SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind id " << id << " to SELECT statement: " << sqlite3_errmsg(_db));
+	SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind id " << id << " to SELECT statement: " << sqlite3_errmsg(_db->db()));
 	// Execute the bound select statement to retrieve the row.
 	result = sqlite3_step(_sStmt);
 	// If the select was successful, this will return SQLITE_ROW. Otherwise, the frame is not in the database.
-	SM_ASSERT_EQ(SqlException, result, SQLITE_ROW, "Unable to execute SELECT statement: " << sqlite3_errmsg(_db));
+	SM_ASSERT_EQ(SqlException, result, SQLITE_ROW, "Unable to execute SELECT statement: " << sqlite3_errmsg(_db->db()));
 	
 	SM_ASSERT_GT(SqlException, sqlite3_column_bytes(_sStmt, 0), 0, "The SELECT statement returned zero bytes.");
 	//std::cout << "Loading " << sqlite3_column_bytes(_sStmt, 0) << " bytes\n";
@@ -99,7 +110,7 @@ namespace sm {
     // After executing, we have to reset the statement
     // http://www.sqlite.org/c3ref/step.html
     result = sqlite3_reset(_sStmt);
-    SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to reset the SELECT  statement: " << sqlite3_errmsg(_db));
+    SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to reset the SELECT  statement: " << sqlite3_errmsg(_db->db()));
     
   }
 
@@ -129,12 +140,12 @@ namespace sm {
       // Step 2 is to bind the frameId and data to the insert statement.
       // http://sqlite.org/c3ref/bind_blob.html
       result = sqlite3_bind_int64(_iStmt, 1, id);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind id " << id << " to INSERT statement: " << sqlite3_errmsg(_db));
+      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind id " << id << " to INSERT statement: " << sqlite3_errmsg(_db->db()));
       result = static_cast<int>(sqlite3_bind_blob(_iStmt, 2, oss.str().c_str(), oss.str().size(), SQLITE_TRANSIENT));
-      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind blob of size " << oss.str().size() << " to INSERT statement: " << sqlite3_errmsg(_db));
+      SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to bind blob of size " << oss.str().size() << " to INSERT statement: " << sqlite3_errmsg(_db->db()));
       // Finally, we execute the bound insert statement.
       result = sqlite3_step(_iStmt);
-      SM_ASSERT_EQ(SqlException, result, SQLITE_DONE, "Unable to execute INSERT statement for id " << id << " and blob of size " << oss.str().size() << ": " << sqlite3_errmsg(_db));
+      SM_ASSERT_EQ(SqlException, result, SQLITE_DONE, "Unable to execute INSERT statement for id " << id << " and blob of size " << oss.str().size() << ": " << sqlite3_errmsg(_db->db()));
 	
     }
     catch(const SqlException & e)
@@ -146,7 +157,7 @@ namespace sm {
     // After executing, we have to reset the statement
     // http://www.sqlite.org/c3ref/step.html
     result = sqlite3_reset(_iStmt);
-    SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to reset the INSERT  statement: " << sqlite3_errmsg(_db));
+    SM_ASSERT_EQ(SqlException, result, SQLITE_OK, "Unable to reset the INSERT  statement: " << sqlite3_errmsg(_db->db()));
   }
 
   template<typename T, typename A>
