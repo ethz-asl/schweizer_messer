0ef73419050bcfe9c5c0aeaf0516f790088d0967
furgalep <paul.furgale@gmail.com>
2013-03-19 13:45:19 +0100

    Removed pose graph and console

--- sm_pose_graph/src/sm_pose_graph/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import the numpy to Eigen type conversion.
-import roslib; roslib.load_manifest('numpy_eigen'); import numpy_eigen
-# Import the the C++ exports from your package library.
-from libsm_pose_graph_python import *
-# Import other files in the directory
-# from mypyfile import *
