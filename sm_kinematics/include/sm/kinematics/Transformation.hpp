1d2f687d246aa55cfca6fe283da13adbf3d8a6df
Paul Furgale <paul.furgale@gmail.com>
2013-04-11 13:52:41 +0200

    Added a set method to the transformation

--- sm_kinematics/include/sm/kinematics/Transformation.hpp
+++ sm_kinematics/include/sm/kinematics/Transformation.hpp
@@ -76,6 +76,8 @@ namespace sm {
       /// \brief Set this to a random transformation.
       virtual void setRandom();
       
+        virtual void set( const Eigen::Matrix4d & T_a_b );
+
       /// \brief Set this to a random transformation with bounded rotation and translation.
       virtual void setRandom( double translationMaxMeters, double rotationMaxRadians);
       /// \brief Set this transformation to identity
