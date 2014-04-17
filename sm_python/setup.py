5131d45d1e24d4b6fd1ab0d08e1d9b1f5c28eca5
Paul Furgale <paul.furgale@gmail.com>
2014-04-17 06:23:05 +0200

    Fixing a typo in the setup.py

--- sm_python/setup.py
+++ sm_python/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['src'],
-    package_dir={'':''})
+    packages=['sm'],
+    package_dir={'':'python'})
 
 setup(**setup_args)
