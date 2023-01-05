13dc372dc2a4f3a4fac1f85ca7575ee86d8dae38
Yannick Huber <yannick.huber@sevensense.ch>
2023-01-05 08:44:36 +0100

    abstract as non copyable

--- sm_python/src/exportValueStoreRef.cpp
+++ sm_python/src/exportValueStoreRef.cpp
@@ -23,8 +23,7 @@ void exportValueStoreRef()
   /// brief create a value store reference pointing to a fresh empty value store (based on @see sm::BoostPropertyTree)
   class_<ValueStoreRef>("ValueStoreRef", init<>())
     .def(init<ValueStoreRef>())
-    .def(init<LayeredValueStore&>())
-    .def(init<PrefixedValueStore&>())
+    .def(init<ValueStore&>())
     .def(init<ValueStore::SharedPtr>())
     .def(init<sm::PropertyTree>())
     /// ValueStoreRef getChild(const std::string & key)
@@ -126,11 +125,19 @@ void exportExtendibleKeyValueStorePair()
   ;
 }
 
+void exportValueStore() {
+  using namespace boost::python;
+  using namespace sm::value_store;
+  
+  class_<ValueStore, boost::noncopyable>("ValueStore", no_init)
+  ;
+}
+
 void exportLayeredValueStore() {
   using namespace boost::python;
   using namespace sm::value_store;
   
-  class_<LayeredValueStore>("LayeredValueStore", init<>())
+  class_<LayeredValueStore, bases<ValueStore>>("LayeredValueStore", init<>())
     .def(init<sm::ValueStoreRef>())
     //void add(ValueStoreRef & p)
     .def<void (LayeredValueStore::*)(ValueStoreRef &)>("add", &LayeredValueStore::add)
@@ -146,5 +153,5 @@ void exportPrefixedValueStore() {
   .value("REMOVE", PrefixedValueStore::PrefixMode::REMOVE)
   ;
 
-  class_<PrefixedValueStore>("PrefixedValueStore", init<const ValueStoreRef&, PrefixedValueStore::PrefixMode, const std::string&>());
+  class_<PrefixedValueStore, bases<ValueStore>>("PrefixedValueStore", init<const ValueStoreRef&, PrefixedValueStore::PrefixMode, const std::string&>());
 }
